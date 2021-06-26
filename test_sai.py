ac288dabf96ab46ed5e4f153924176403a82b29b
Saisai Zhang <1019838439@qq.com>
2021-06-26 20:44:17 +0800

    Update test_sai.py

--- test_sai.py
+++ test_sai.py
@@ -15,4 +15,12 @@ if __name__ == '__main__':
             x = (x / 4) * 5 + 1
         j += 1
     print
-    
+
+    import time
+
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+    # 暂停一秒
+    time.sleep(1)
+
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
